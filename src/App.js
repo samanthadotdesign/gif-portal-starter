37a60eda2c3cb4d1ce8ba00e976888b7c1eeca15
samanthadotdesign <samleeml@gmail.com>
2021-11-07 14:31:28 +0800

    created initial app using react with wallet authentication and connected solana

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -5,15 +6,122 @@ import './App.css';
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+const TEST_GIFS = [
+	'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
+	'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
+	'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
+	'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+]
+
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState(''); //input box value changes 
+
+  /*
+   * This function holds the logic for deciding if a Phantom Wallet is
+   * connected or not
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+
+          /*
+         * The solana object gives us a function that will allow us to connect
+         * directly with the user's wallet!
+         */
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log(
+          'Connected with Public Key:',
+          response.publicKey.toString()
+        );
+      
+
+        /*
+         * Set the user's publicKey in state to be used later!
+         */
+        setWalletAddress(response.publicKey.toString());
+        }
+
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  useEffect(() => {
+    window.addEventListener('load', async (event) => {
+      await checkIfWalletIsConnected();
+    });
+  }, []);
+
+  const connectWallet = async () => {};
+
+  const onInputChange = (event) => {
+  const { value } = event.target;
+  setInputValue(value);
+};
+
+const sendGif = async () => {
+  if (inputValue.length > 0) {
+    console.log('Gif link:', inputValue);
+  } else {
+    console.log('Empty input. Try again.');
+  }
+};
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+ const renderConnectedContainer = () => (
+  <div className="connected-container">
+    {/* Go ahead and add this input and button to start */}
+    <input
+      type="text"
+      placeholder="Enter gif link!"
+      value={inputValue}
+      onChange={onInputChange}
+    />
+    <button className="cta-button submit-gif-button" onClick={sendGif}>
+      Submit
+    </button>    
+    <div className="gif-grid">
+      {TEST_GIFS.map((gif) => (
+        <div className="gif-item" key={gif}>
+          <img src={gif} alt={gif} />
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
   return (
     <div className="App">
-      <div className="container">
+     <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">🖼 SQUID GAME GIFS PORTAL</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+          {/* Add the condition to show this only if we don't have a wallet address */}
+          {!walletAddress && renderNotConnectedContainer()}
+          {/* We just need to add the inverse here! */}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
